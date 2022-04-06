c82bb851e8d02991c56d26e12310298b2e9d8d57
Pavel Taruts <ptrtss@gmail.com>
2022-04-06 19:05:32 +0300

    Force clone

--- src/main/java/we/gitUtils/GitUtils.java
+++ src/main/java/we/gitUtils/GitUtils.java
@@ -11,24 +11,59 @@ import we.processUtils.ProcessRunner;
 import java.io.File;
 
 @Slf4j
-public class GitCloner {
+public class GitUtils {
+
+    public static void cloneOrUpdate(String url, File directory) {
+        cloneOrUpdate(url, null, null, directory);
+    }
+
+    public static void cloneOrUpdate(String url, String accessToken, File directory) {
+        cloneOrUpdate(url, accessToken, null, directory);
+    }
 
     @SneakyThrows
-    public static void cloneOrUpdateGitRepository(String url, String username, String password, File directory) {
+    public static void cloneOrUpdate(String url, String username, String password, File directory) {
         if (directory.exists()) {
             try {
-                updateGitRepository(directory);
+                updateWorkingTree(directory);
             } catch (Exception e) {
                 log.error("Could not update git repository", e);
                 FileUtils.deleteDirectory(directory);
-                cloneGitRepository(url, username, password, directory);
+                clone(url, username, password, directory);
             }
         } else {
-            cloneGitRepository(url, username, password, directory);
+            clone(url, username, password, directory);
         }
     }
 
-    private static void cloneGitRepository(String url, String username, String password, File directory) {
+    public static void forceClone(String url, File directory) {
+        forceClone(url, null, null, directory);
+    }
+
+    public static void forceClone(String url, String accessToken, File directory) {
+        forceClone(url, accessToken, null, directory);
+    }
+
+    @SneakyThrows
+    public static void forceClone(String url, String username, String password, File directory) {
+        directory = new File(directory.getCanonicalPath());
+
+        if (directory.exists()) {
+            if (!FileUtils.deleteQuietly(directory)) {
+                throw new RuntimeException((
+                        "Could not delete directory %s. It must be blocked by a process. " +
+                                "Restarting the IDE sometimes helps"
+                ).formatted(directory));
+            }
+        }
+        if (!directory.getParentFile().mkdirs()) {
+            throw new RuntimeException("Could not create directory ");
+        }
+
+        clone(url, username, password, directory);
+    }
+
+    private static void clone(String url, String username, String password, File directory) {
         if (StringUtils.isNotBlank(username)) {
             url = addCredentialsToGitRepositoryUrl(url, username, password);
         }
@@ -60,7 +95,7 @@ public class GitCloner {
         return sb.toString();
     }
 
-    private static void updateGitRepository(File directory) {
+    private static void updateWorkingTree(File directory) {
         ProcessRunner.runProcess(directory, "git", "fetch");
         ProcessRunner.runProcess(directory, "git", "pull");
     }
